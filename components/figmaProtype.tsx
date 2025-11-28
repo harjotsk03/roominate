234e6041cb59dc198e7f0950219696d983dd25fa
Harjot Singh <harjotsk03@gmail.com>
2025-11-27 16:27:30 -0800

    change prototype link

--- components/figmaProtype.tsx
+++ components/figmaProtype.tsx
@@ -5,7 +5,7 @@ export default function FigmaPrototype() {
         style={{ border: "none" }}
         width="100%"
         height="100%"
-        src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/IejRnDYsE1Xk5nweB3Tv4d/iat-334-Final-project?page-id=1130%3A53&node-id=1130-8085&p=f&viewport=549%2C173%2C0.06&t=FeCsznlrtcyvMzZ7-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=1130%3A8085&show-proto-sidebar=0"
+        src="https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/IejRnDYsE1Xk5nweB3Tv4d/iat-334-Final-project?page-id=2722%3A16813&node-id=2822-29226&p=f&viewport=279%2C40%2C0.03&t=ZCFexKyKF5PjJoHw-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2822%3A29226&show-proto-sidebar=1"
         allowFullScreen
       />
     </div>
